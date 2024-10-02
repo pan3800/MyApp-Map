4f5429edac2c894265cccc5ded719b21b43419eb
pan3800 <pan173434@gmail.com>
2024-10-02 14:06:16 +0900

    프로젝트 연결

---
+++ MyApp-Map/MyApp-Map/MyApp_MapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyApp_MapApp.swift
+//  MyApp-Map
+//
+//  Created by 임채성 on 2024/10/02.
+//
+
+import SwiftUI
+
+@main
+struct MyApp_MapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
