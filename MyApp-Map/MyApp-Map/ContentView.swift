1fece48c35d671fad5c41d1cc72ce2cfc78d7312
pan3800 <pan173434@gmail.com>
2024-10-06 21:54:40 +0900

    MainTabView 파일 추가

--- MyApp-Map/MyApp-Map/ContentView.swift
+++ MyApp-Map/MyApp-Map/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
     var body: some View {
         VStack {
             if authManager.signState == .signIn {
-                ProfileView()
+                MainTabView()
             } else {
                 LoginView()
             }
---
+++ MyApp-Map/MyApp-Map/MainTabView.swift
@@ -0,0 +1,38 @@
+//
+//  MainTabView.swift
+//  MyApp-Map
+//
+//  Created by 임채성 on 2024/10/06.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    @State var tabIndex = 2
+    
+    var body: some View {
+        TabView(selection: $tabIndex) {
+            Text("Feed")
+                .tabItem {
+                    Image(systemName: "house")
+                }
+                .tag(0)
+            Text("Search")
+                .tabItem {
+                    Image(systemName: "magnifyingglass")
+                }
+                .tag(1)
+            ProfileView()
+                .tabItem {
+                    Image(systemName: "person.circle")
+                }
+                .tag(2)
+        }
+    }
+}
+
+struct MainTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainTabView()
+    }
+}
